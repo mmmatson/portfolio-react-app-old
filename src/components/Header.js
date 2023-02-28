e985a982da6bd0ff338f30d56136529ad5d38dd5
mmm <margaret.m.matson@gmail.com>
2023-02-27 21:57:29 -0600

    added info to about me page and image

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../logo.png';
 
 function Header({ currentPage, handlePageChange }) {
 
@@ -9,7 +10,7 @@ function Header({ currentPage, handlePageChange }) {
         <>
             <Navbar className="nav-container" bg="dark" variant="dark">
                 <Container>
-                    <Nav className="me-auto">
+                <Nav className="me-auto">
                         <Navbar.Brand id="menu" href="#portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Navbar.Brand>
                         <Navbar.Brand id="menu" href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</Navbar.Brand>
                         <Navbar.Brand id="menu" href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About Me</Navbar.Brand>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,28 +3,31 @@ import Form from 'react-bootstrap/Form';
 
 function Contact() {
   return (
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Enter name" />
-          </Form.Group>
-          
-          <Form.Group className="mb-3" controlId="formBasicName">
+    <div>
+      <h1>Contact</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Enter name" />
+        </Form.Group>
 
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-    
-          <Form.Group className="mb-3" controlId="formBasicMessage">
-            <Form.Label>Message</Form.Label>
-            <Form.Control type="message" placeholder="Message" />
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      );
-    }
-    
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control type="message" placeholder="Message" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
 export default Contact;
