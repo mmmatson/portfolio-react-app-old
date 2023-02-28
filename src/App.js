26b8026afab788a49e8d6ead1518088b8665ef4a
mmm <margaret.m.matson@gmail.com>
2023-02-27 21:27:40 -0600

    set up pages and navigation, added contact form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Content from './components/Content';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +22,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Content />
       <Footer />
     </div>
   )
